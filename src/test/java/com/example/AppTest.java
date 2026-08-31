bd0677cf8c56e917cd740e392276c587d306abe4
Jean Wang <jean@Jeans-MacBook-Pro.local>
2026-08-31 18:41:23 +0800

    added divide fn and dividebyzero unit test
    added divide & arith error

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
@@ -9,5 +11,13 @@ public class AppTest {
     Calculator calculator = new Calculator();
     int result = calculator.add(5, 3);
     assertEquals(8, result);
+  }
+
+  @Test
+  void shouldThrowExceptionWhenDividingByZero() {
+    Calculator calculator = new Calculator();
+    assertThrows(ArithmeticException.class, () -> {
+      calculator.divide(10, 0);
+    });
   }
 }
